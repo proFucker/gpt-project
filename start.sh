9f5e97cf82c18decdeabc7a413ba082266210fc6
绯村剑一 <1093610510@qq.com>
2023-04-15 01:09:06 +0800

    aaa

--- start.sh
+++ start.sh
@@ -25,4 +25,4 @@ mv ./gpt-app/build/libs/gpt-app-1.0.jar
 gradleProcessId=`ps -ef | grep java | grep -i "gradle" | awk 'BEGIN{FS=" "}{print $2}'`
 kill $gradleProcessId
 
-nohup $JAVA_HOME/java -server -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC  -Xms128m -Xmx128m -jar ./gpt-app/build/libs/gpt-app-1.0.jar --spring.profiles.active=prod > myout.file 2>&1 &
+nohup $JAVA_HOME/bin/java -server -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC  -Xms128m -Xmx128m -jar ./gpt-app/build/libs/gpt-app-1.0.jar --spring.profiles.active=prod > myout.file 2>&1 &
