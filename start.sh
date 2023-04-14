9011d02c8683abf5cee728bcfc3184079c688408
绯村剑一 <1093610510@qq.com>
2023-04-15 01:05:56 +0800

    aa

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 appProcessId=`ps -ef | grep java | grep "gpt-app" | awk 'BEGIN{FS=" "}{print $2}'`
 
+echo $appProcessId
+
 if  [ x"$appProcessId" != "" ];then
   kill $$appProcessId
 fi
@@ -7,17 +9,20 @@ fi
 projectHead=$1
 
 if  [ x"$projectHead" != "" ];then
+  echo "tag"
   git checkout $projectHead
   git pull
 else
+  echo "master"
   git checkout master
   git pull
 fi
 
-./gradlew :gpt-app:clean
+#./gradlew :gpt-app:clean
+mv ./gpt-app/build/libs/gpt-app-1.0.jar
 ./gradlew :gpt-app:bootJar
 
-gradleProcessId=`ps -ef | grep java | grep "gpt-app" | awk 'BEGIN{FS=" "}{print $2}'`
+gradleProcessId=`ps -ef | grep java | grep -i "gradle" | awk 'BEGIN{FS=" "}{print $2}'`
 kill $gradleProcessId
 
 nohup $JAVA_HOME/java -server -XX:-OmitStackTraceInFastThrow -XX:+UseG1GC  -Xms128m -Xmx128m -jar ./gpt-app/build/libs/gpt-app-1.0.jar --spring.profiles.active=prod > myout.file 2>&1 &
